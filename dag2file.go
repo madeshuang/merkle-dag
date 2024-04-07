7fb5da861758e595b545d0096e9e1a19c9c056e9
horsegetcool <1469912842@qq.com>
2024-04-07 15:16:48 +0800

    update dag2file.go

--- dag2file.go
+++ dag2file.go
@@ -1,7 +1,18 @@
 package merkledag
 
+import (
+	"fmt"
+)
+
 // Hash to file
 func Hash2File(store KVStore, hash []byte, path string, hp HashPool) []byte {
-	// 根据hash和path， 返回对应的文件, hash对应的类型是tree
-	return nil
+	// 根据 hash 从 KVStore 中获取数据
+	data, err := store.Get(hash)
+	if err != nil {
+		// 处理错误
+		fmt.Println("Error:", err)
+		return nil
+	}
+	// 返回获取到的数据
+	return data
 }
